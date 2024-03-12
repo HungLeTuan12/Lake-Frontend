2d49365de5d12aa950b597e2c48e8e75137fd401
Hung Le <hunglehung812@gmail.com>
2024-03-12 11:26:05 +0700

    order and order card

---
+++ src/customer/components/Order/OrderCard.jsx
@@ -0,0 +1,52 @@
+import { Grid } from "@mui/material";
+import React from "react";
+import AdjustIcon from "@mui/icons-material/Adjust";
+const OrderCard = () => {
+  return (
+    <div className="p-5 border shadow-lg hover:shadow-2xl">
+      <Grid container spacing={2} sx={{ justifyContent: "space-between" }}>
+        {/* Image and infor */}
+        <Grid item xs={6}>
+          <div className="flex cursor-pointer">
+            <img
+              className="w-[5rem] h-[5rem] object-cover object-top"
+              src="https://static.nike.com/a/images/t_PDP_1280_v1/f_auto,q_auto:eco/fdded470-0ac5-4bd7-b41b-1bb63e161438/custom-nike-air-force-1-mid-by-you-shoes.png"
+              alt=""
+            />
+            <div className="ml-5 space-y-2">
+              <p className="">Nike Air Force 1 Mid By You</p>
+              <p className="opacity-50 text-xs font-semibold">Size: M</p>
+              <p className="opacity-50 text-xs font-semibold">Color: Black</p>
+            </div>
+          </div>
+        </Grid>
+        {/* Price */}
+        <Grid item xs={2}>
+          <p>$1099</p>
+        </Grid>
+        {/* Price */}
+        <Grid item xs={4}>
+          {true && (
+            <div>
+              <p>
+                <AdjustIcon
+                  sx={{ width: "15px", height: "15px" }}
+                  className="text-green-600 mr-2 text-sm"
+                />
+                <span>Delivered On March 03</span>
+              </p>
+
+              <p className="text-xs">Your item has been delivered</p>
+            </div>
+          )}
+          {false && (
+            <p>
+              <span>Your item has been delivered</span>
+            </p>
+          )}
+        </Grid>
+      </Grid>
+    </div>
+  );
+};
+export default OrderCard;
